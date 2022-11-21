82ab1ae77d79a561a3a056128ca55b2ec9741bcf
robert@jbcole.co.uk <robert@jbcole.co.uk>
2022-11-21 18:52:29 +0100

    minor bug fix

--- src/pages/payment/Payment.js
+++ src/pages/payment/Payment.js
@@ -99,7 +99,7 @@ function getPayment() {
                 paymentTotal={price}
               />
               <div style={{"padding": "21px"}}>
-              <Link to="/3f0TEST309"><Button variant={'outlined'} className='payment-btn'>Pay Now</Button></Link>
+              <Button onClick={() => setPaymentModalOpen(true)} variant={'outlined'} className='payment-btn'>Pay Now</Button>
               </div>
             </div>
           </div>
